7585958ea427e73a984d2455213d77010b49e91e
Nenad Zivkovic <nenad@freetuts.org>
2015-01-03 21:06:21 +0100

    Adds tranlsation support

--- _protected/translations/sr/yii.php
+++ _protected/translations/sr/yii.php
@@ -2,5 +2,6 @@
 
 return [
     '{attribute} cannot be blank.' => 'polje {attribute} ne sme biti prazno.',
+    'The verification code is incorrect.' => 'Antispam kod nije tačan.',
     'Home' => 'Početna',
 ];
