ff5e80e8398a4159b1e1b93dd28a088a09ce7986
Nenad Zivkovic <nenad@freetuts.org>
2015-01-08 16:56:18 +0100

    Kraken is released!

--- _protected/translations/sr/yii.php
+++ _protected/translations/sr/yii.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+
     '{attribute} cannot be blank.' => 'polje {attribute} ne sme biti prazno.',
     'The verification code is incorrect.' => 'Antispam kod nije tačan.',
     'Home' => 'Početna',
