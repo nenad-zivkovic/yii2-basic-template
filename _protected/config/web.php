8d069043d9ffd05349c282de529c6ec77998db47
Arief Wijaya <hoaaah.arief@gmail.com>
2018-07-06 16:51:41 +0700

    Update web.php

--- _protected/config/web.php
+++ _protected/config/web.php
@@ -24,6 +24,7 @@ $config = [
             'bundles' => [
                 // we will use bootstrap css from our theme
                 'yii\bootstrap\BootstrapAsset' => [
+                    'css' => [], // do not use yii default one
                 ],
             ],
         ],
